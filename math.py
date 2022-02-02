4e99b40cc19c92e9d6cf98232c07b96dfaf53023
ShubhamChouhan538 <88274056+ShubhamChouhan538@users.noreply.github.com>
2022-02-02 12:49:52 +0530

    Initial Commit of math.py

---
+++ math.py
@@ -0,0 +1,24 @@
+#Add implementation
+def add(x,y):
+    pass
+
+#Subtract implementation
+def subtract(x,y):
+    return x-y          #on master branch
+    
+#Multiply implementation    
+def multiply(x,y):
+    return x*y            #on Bug456 Branch
+   
+#Divide implementation   
+def divide(x,y):
+    if y==0
+        return DIVIDE_BY_0_ERROR
+    else
+        return x/y                 #on master branch
+    
+def mode(x,y):
+    pass
+    
+def square(x):
+    return x*x
